357edf6067354ee56d14b8c82d58193b6535a345
sorokin.vlad <623564@gmail.com>
2021-01-30 07:03:42 +0300

    30.01

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -5,7 +5,7 @@ import {sidebarReducer} from "./sidebar_reducer";
 import usersReducer from "./users_reducer";
 import authReducer from "./auth_reducer";
 import thunk  from "redux-thunk";
-
+import {reducer as formReducer} from 'redux-form'
 
 
 let reducers = combineReducers({
@@ -13,7 +13,8 @@ let reducers = combineReducers({
     dialogsPage:dialogsReducer,
     sidebar:sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunk));
